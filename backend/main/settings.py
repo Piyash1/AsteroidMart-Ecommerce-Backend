6354a53cf215f99fbbf6323861dee0306668f4ee
Piyash1 <moniruzzamanpiyash@gmail.com>
2025-09-04 00:04:20 +0600

    updated cors origin

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -52,6 +52,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React development server
     "http://localhost:8080",  # Vue development server
     "http://127.0.0.1:3000",  # Alternative localhost format
+    "https://asteroidmart.vercel.app",
 ]
 
 ROOT_URLCONF = 'main.urls'
